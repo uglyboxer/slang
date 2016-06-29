e5d7f2df50926e2fa42c88952075099640af4a43
Cole Howard <uglyboxer@gmail.com>
2016-06-29 07:41:13 -0700

    Add spanish search

--- dict/views.py
+++ dict/views.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+from lxml import html
+
 from django.shortcuts import render
 from django.db.models import Q
 
@@ -14,7 +16,9 @@ def home_page(request):
 
 
 def search(request):
-    search_term = request.GET['q']
+    # search_term = request.GET['q']
+    search_term = request.POST['search-term']
+        
     # qset = Q()
     # for term in search_term.split():
     #     qset |= Q(name__contains=term)
@@ -33,14 +37,47 @@ def search(request):
 
     except:
         
-        url = 'http://api.urbandictionary.com/v0/define?term=%s' % (search_term)
+        if not request.POST.get('spanish', ''):
+            url = 'http://api.urbandictionary.com/v0/define?term=%s' % (search_term)
 
-        r = requests.get(url)
-        data = r.json()
-        ret_def = data['list'][0]['definition']
-        entry = Entry(word=search_term, definition=ret_def, response1='',
-                      response2='')
-        entry.save()
-        return render(request, 'home.html', {'title': 'Slictionary',
-                                             'word': search_term, 
-                                             'definition': ret_def})
+            r = requests.get(url)
+            data = r.json()
+            ret_def = data['list'][0]['definition']
+            entry = Entry(word=search_term, definition=ret_def, response1='',
+                          response2='')
+            entry.save()
+            return render(request, 'results.html', {'title': 'Slictionary',
+                                                 'word': search_term, 
+                                                 'definition': ret_def})
+
+        else:
+            page = requests.get(
+                'http://www.asihablamos.com/word/palabra/{}.php?pais=MX'\
+                .format(search_term))  
+            tree = html.fromstring(page.content)
+            ret_def = tree.xpath('//div[@class="definicion"]/div[2]/text()')[0]
+
+            # Translate
+            # 
+            # Insert into db
+            
+            return render(request, 'results.html', {'title': 'Slictionary',
+                                                 'word': search_term, 
+                                                 'definition': ret_def})
+
+
+### Check db
+## If not in:
+## determine Spanish/English
+## Eng use above
+## Spanish use below
+
+#### Spanish
+## from asihablamos.com
+## 
+## from lxml import html
+## page = requests.get('http://www.asihablamos.com/word/palabra/{{ search term }}.php?pais=MX')  
+## tree = html.fromstring(page.content)
+## definition = tree.xpath('//div[@class="definicion"]/div[2]/text()')
+## TRANSLATE w/ Google API
+## Insert into db
