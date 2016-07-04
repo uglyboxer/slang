3fc3d4fda90acc6b0a1a88cea0ffd6687ec7ac9e
Cole Howard <uglyboxer@gmail.com>
2016-07-04 11:40:08 -0700

    Frontend

--- slang/settings.py
+++ slang/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-from secrets import *
+from slang.secrets import *
 
 if not SECRET_KEY:
     SECRET_KEY = os.environ.get('SECRET_KEY', '')
