224a87b94a2653af44bbe8b3b3306e1e3a305448
Cole Howard <uglyboxer@gmail.com>
2016-07-04 17:48:49 -0700

    New config

--- slang/settings.py
+++ slang/settings.py
@@ -133,6 +133,6 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'dict', 'static'),
 )
 
